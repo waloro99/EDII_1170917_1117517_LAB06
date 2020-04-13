06959e33906ee6929313ccb21853bfff80bb034c
waloro <walterof1@hotmail.com>
2020-04-13 15:27:03 -0600

    Create first proyect the API

---
+++ LAB06_ED2/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LAB06_ED2
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
